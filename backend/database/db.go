49c3f041b5803fb54a2c5cbf488f9885a1404704
Sivaramakrishnan S <sivaramakrishnans@Sivaramakrishnans-MacBook-Pro.local>
2025-03-31 12:03:08 -0400

    changed the port

--- backend/database/db.go
+++ backend/database/db.go
@@ -13,7 +13,7 @@ var DB *gorm.DB // Global variable to hold the database connection
 
 // ConnectDB initializes the PostgreSQL database connection
 func ConnectDB() *gorm.DB {
-	dsn := "host=localhost user=postgres dbname=postgres password=swamp port=5432 sslmode=disable"
+	dsn := "host=localhost user=postgres dbname=postgres password=swamp port=5433 sslmode=disable"
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal("Failed to connect to the database:", err)
