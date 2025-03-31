ea19e77ef7d718c4b72e759af1003ffa564fed68
Sivaramakrishnan S <sivaramakrishnans@Sivaramakrishnans-MacBook-Pro.local>
2025-03-31 10:04:33 -0400

    added db

--- backend/database/db.go
+++ backend/database/db.go
@@ -3,7 +3,7 @@ package database
 import (
 	"fmt"
 	"log"
-	"swamp/models"
+	//"swamp/models"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -13,14 +13,14 @@ var DB *gorm.DB // Global variable to hold the database connection
 
 // ConnectDB initializes the PostgreSQL database connection
 func ConnectDB() *gorm.DB {
-	dsn := "host=localhost user=postgres dbname=postgres password=swamp port=5433 sslmode=disable"
+	dsn := "host=localhost user=postgres dbname=postgres password=swamp port=5432 sslmode=disable"
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal("Failed to connect to the database:", err)
 	}
 
 	// AutoMigrate all models
-	err = db.AutoMigrate(&models.User{}, &models.OTP{})
+	//err = db.AutoMigrate(&models.User{}, &models.OTP{})
 	if err != nil {
 		log.Fatal("Failed to migrate database:", err)
 	}
